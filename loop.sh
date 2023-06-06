d6951419774ac62c4c09cbf784a85a674df4eb65
keerthi reddy <keerthireddi01@gmail.com>
2023-06-06 11:55:54 +0530

    xasc

--- loop.sh
+++ loop.sh
@@ -4,3 +4,10 @@ while [ $a -gt 0 ]; do
    a=$(($a-1))
    sleep 2
  done  
+
+ #creating componets servers using loops
+
+ for component in catalogue user cart frontend shipping payment ; do
+  echo creating component - $component
+  sleep 1
+ done 
